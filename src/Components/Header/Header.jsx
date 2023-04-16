99b481e9bf675541edb4b616b025c20b1fc15d2c
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-04-16 10:22:11 +0100

    stopped scrolling when nav open, and increased the width of the navbar for specific screen sizes 355px down

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,39 @@
 import NavBar from './NavBar';
 import Hero from './Hero';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+  //false = closed true = open
+  const [navState, setNavState] = useState(false);
+
+  const [fixed, setFixed] = useState(false);
+  const body = document.querySelector('body');
+
+  useEffect(() => {
+    if (fixed) {
+      body.classList.add('becomeFixed');
+      document.documentElement.classList.add('becomeFixed');
+    } else {
+      body.classList.remove('becomeFixed');
+      document.documentElement.classList.remove('becomeFixed');
+    }
+  }, [fixed]);
+
+  const handleNav = () => {
+    setNavState((prevState) => !prevState);
+    setFixed((prevState) => !prevState);
+  };
+
   return (
-    <header id="home" className="relative w-full">
-      <div className="nav flex items-center justify-center bg-navBar">
-        <NavBar />
-      </div>
-      <Hero />
-    </header>
+    <>
+      <header id="home" className="relative w-full">
+        <div className="nav relative z-[9999] flex items-center justify-center bg-navBar">
+          <NavBar navState={navState} handleNav={handleNav} />
+        </div>
+        <Hero />
+      </header>
+
+      <div className={`overlay fixed left-0 top-0 z-[6000] ${!navState ? 'hidden' : ''} h-full w-full bg-[#33333333]`}></div>
+    </>
   );
 }
